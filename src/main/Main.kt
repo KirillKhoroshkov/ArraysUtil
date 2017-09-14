7fef45288bebafd4bd970fa9d15613712a80b49d
khoroshkovkirill <khoroshkov_kirill@mail.ru>
2017-09-14 15:27:57 +0300

    bubbleSort

--- src/main/Main.kt
+++ src/main/Main.kt
@@ -3,8 +3,29 @@ class Main {
 
     companion object {
 
-        @JvmStatic fun Sort(array: Array<Int>) {
+        @JvmStatic fun bubbleSort(array: Array<Int>) {//Пузырек с флагом
+            var flag : Boolean
+            var box : Int
+            var lastIndex = array.size - 2
+            do {
+                flag = false
+                for (j in 0..lastIndex){
+                    if (array[j] > array[j + 1]){
+                        flag = true
+                        box = array[j]
+                        array[j] = array[j + 1]
+                        array[j + 1] = box
+                    }
+                }
+                lastIndex--
+            } while (flag)
+        }
 
+        @JvmStatic fun insertSort(array: Array<Int>) {
+            var box : Int
+            var sortedIndex = 0
+            var currentIndex : Int
+            
         }
 
         @JvmStatic fun toString (array: Array<Int>): String{
--- src/test/Tests.kt
+++ src/test/Tests.kt
@@ -11,4 +11,12 @@ class Tests {
         assertEquals(Main.toString(array), "0, 1, 2, 3, 4, 5, 6, 7, 8, 9")
     }
 
+    @Test fun bubbleSortTest(){
+        val testArray = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        val array = arrayOf(9, 3, 6, 1, 4, 5, 2, 7, 8, 0)
+        Main.bubbleSort(array)
+        println(Main.toString(array))
+        assertEquals(array, testArray)
+    }
+
 }
